2111409f23c0915bd1f93862b20f1506b2beff06
jdmalham <jdmalham100@gmail.com>
2023-04-17 17:31:49 -0700

    Beginning of logic to handle compressed IG files as user input

---
+++ ConsoleApp1/Unzip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGtoOBJGen
+{
+    internal class Unzip
+    {
+        public static void unzipIG(string path)
+        {
+            string extractPath = tempDirectoryPath();
+            ZipFile.ExtractToDirectory(path, extractPath);
+
+        }
+        private static string tempDirectoryPath()
+        {
+            string tempFolder = Path.GetTempFileName();
+            File.Delete(tempFolder);
+            Directory.CreateDirectory(tempFolder);
+
+            return tempFolder;
+        }
+    }
+}
