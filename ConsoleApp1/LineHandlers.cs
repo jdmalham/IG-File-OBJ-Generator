cfec64406e607839fccb4beef9cc77b87928b0d2
jdmalham <jdmalham100@gmail.com>
2023-07-26 12:51:47 -0700

    changes to how tracks get parsed and saved as files

--- ConsoleApp1/LineHandlers.cs
+++ ConsoleApp1/LineHandlers.cs
@@ -31,6 +31,10 @@ namespace IGtoOBJGen
 
             if (!Directory.Exists($"{desktopPath}\\{eventTitle}")) { 
                 Directory.CreateDirectory($"{desktopPath}\\{eventTitle}");
+                Directory.CreateDirectory($"{desktopPath}\\{eventTitle}\\standaloneMuons");
+                Directory.CreateDirectory($"{desktopPath}\\{eventTitle}\\globalMuons");
+                Directory.CreateDirectory($"{desktopPath}\\{eventTitle}\\trackerMuons");
+                Directory.CreateDirectory($"{desktopPath}\\{eventTitle}\\gsfElectrons");
             }
 
             Execute();
@@ -59,7 +63,6 @@ namespace IGtoOBJGen
 
             electronData = electronParse();
             makeElectrons();
-            
         }
 
         //Methods
@@ -135,15 +138,18 @@ namespace IGtoOBJGen
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\Photons_V1.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\Photons_V1.obj", dataStrings);
         }
-        public List<string> trackCubicBezierCurve(List<TrackExtrasData> inputData) {
+        public List<string> trackCubicBezierCurve(List<TrackExtrasData> inputData,string objectName) {
             //Calculate the bezier path of the tracks based on the four pos control vectors defined in the TrackExtrasData struct
             List<string> dataList = new List<string>();
+            List<string> testList = new List<string>();
             List<int> exclusion_indeces = new List<int>();
             int numVerts = 32;
             int n = 0;
+            int num = 1;
             foreach (var item in inputData) 
             {
-
+                testList.Clear();
+                dataList.Add("o OBJECT");
                 for (double i = 0.0; i <= numVerts; i++) {
                     
                     double t = (double)(i) / (double)(numVerts);
@@ -168,8 +174,17 @@ namespace IGtoOBJGen
                     string point_t2 = $"v {point[0]} {point[1] + 0.001} {point[2]}";
 
                     dataList.Add(poin_t); dataList.Add(point_t2);
+                    testList.Add(poin_t); testList.Add(point_t2);
                     n += 2;
                 }
+               for (int r=1; r<n-2; r++)
+                {
+                    string faces1 = $"f {r} {r + 1} {r + 3} {r + 2}";
+                    string faces2 = $"f {r + 2} {r + 3} {r + 1} {r}";
+                    testList.Add(faces1);testList.Add(faces2);
+                }
+                File.WriteAllLines(desktopPath + $"/{eventTitle}" + $"/{objectName}_{num}.obj", testList);
+                num++;
                 exclusion_indeces.Add(n);
             }
             
@@ -186,6 +201,68 @@ namespace IGtoOBJGen
                 dataList.Add(faces2);
             }
             
+            return dataList;
+            //filePaths.Add($"{desktopPath}\\{eventName}\\tracks.obj");
+        }
+        public List<string> singleFiletrackCubicBezierCurve(List<TrackExtrasData> inputData)
+        {
+            //Same as the above function, except it outputs all the tracks into their own singular file. This allows me to keep Tracks_V3 as its 
+            //own single file since it doesn't need to be matched to data.
+            List<string> dataList = new List<string>();
+            List<string> testList = new List<string>();
+            List<int> exclusion_indeces = new List<int>();
+            int numVerts = 32;
+            int n = 0;
+            int num = 1;
+            foreach (var item in inputData)
+            {
+                testList.Clear();
+                dataList.Add("o OBJECT");
+                for (double i = 0.0; i <= numVerts; i++)
+                {
+
+                    double t = (double)(i) / (double)(numVerts);
+
+                    double t1 = Math.Pow(1.0 - t, 3);
+                    double t2 = 3 * t * Math.Pow(1.0 - t, 2);
+                    double t3 = 3 * t * t * (1.0 - t);
+                    double t4 = Math.Pow(t, 3);
+
+                    // Check out the wikipedia page for bezier curves if you want to understand the math. That's where I learned it!
+                    // also we're using double arrays because i dont like Vector3 and floats. I'm the one who has to go through the headaches of working with double arrays
+                    // instead of Vector3 so i get to make that call. i also wrote this before i realized i couldn't avoid using MathNET and i can't be bothered to 
+                    // change it such that it uses MathNET vectors
+
+                    double[] term1 = { t1 * item.pos1[0], t1 * item.pos1[1], t1 * item.pos1[2] };
+                    double[] term2 = { t2 * item.pos3[0], t2 * item.pos3[1], t2 * item.pos3[2] };
+                    double[] term3 = { t3 * item.pos4[0], t3 * item.pos4[1], t3 * item.pos4[2] };
+                    double[] term4 = { t4 * item.pos2[0], t4 * item.pos2[1], t4 * item.pos2[2] };
+                    double[] point = { term1[0] + term2[0] + term3[0] + term4[0], term1[1] + term2[1] + term3[1] + term4[1], term1[2] + term2[2] + term3[2] + term4[2] };
+
+                    string poin_t = $"v {point[0]} {point[1]} {point[2]}";
+                    string point_t2 = $"v {point[0]} {point[1] + 0.001} {point[2]}";
+
+                    dataList.Add(poin_t); dataList.Add(point_t2);
+                    testList.Add(poin_t); testList.Add(point_t2);
+                    n += 2;
+                }
+                exclusion_indeces.Add(n);
+            }
+
+            for (int r = 1; r <= n - 2; r += 2)
+            {
+                if (exclusion_indeces.Contains(r + 1))
+                {
+                    //Make sure the tracks don't loop back on each other
+                    continue;
+                }
+                // Define faces
+                string faces1 = $"f {r} {r + 1} {r + 3} {r + 2}";
+                string faces2 = $"f {r + 2} {r + 3} {r + 1} {r}";
+                dataList.Add(faces1);
+                dataList.Add(faces2);
+            }
+
             return dataList;
             //filePaths.Add($"{desktopPath}\\{eventName}\\tracks.obj");
         }
@@ -267,8 +344,13 @@ namespace IGtoOBJGen
         }
         public void makeGlobalMuons() 
         {
+            DirectoryInfo dir = new DirectoryInfo($"{desktopPath}\\{eventTitle}\\globalMuons");
+            foreach (var file in dir.GetFiles())
+            {
+                file.Delete();
+            }
             if (globalMuonExtras == null) { return; }
-            List<string> dataList = trackCubicBezierCurve(globalMuonExtras);
+            List<string> dataList = trackCubicBezierCurve(globalMuonExtras,"globalMuons\\2_globalMuons");
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\2_globalMuons.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\2_globalMuons.obj", dataList);
         }
@@ -307,8 +389,13 @@ namespace IGtoOBJGen
         }
         public void makeTrackerMuons() 
         {
+            DirectoryInfo dir = new DirectoryInfo($"{desktopPath}\\{eventTitle}\\trackerMuons");
+            foreach (var file in dir.GetFiles())
+            {
+                file.Delete();
+            }
             if (trackerMuonExtras == null) { return; }
-            List<string> dataList = trackCubicBezierCurve(trackerMuonExtras);
+            List<string> dataList = trackCubicBezierCurve(trackerMuonExtras,"trackerMuons\\1_trackerMuons");
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\1_trackerMuons.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\1_trackerMuons.obj", dataList);
         }
@@ -345,8 +432,13 @@ namespace IGtoOBJGen
         }
         public void makeStandaloneMuons() 
         {
+            DirectoryInfo dir = new DirectoryInfo($"{desktopPath}\\{eventTitle}\\standaloneMuons");
+            foreach (var file in dir.GetFiles())
+            {
+                file.Delete();
+            }
             if (standaloneMuonExtras == null) { return; }
-            List<string> dataList = trackCubicBezierCurve(standaloneMuonExtras);
+            List<string> dataList = trackCubicBezierCurve(standaloneMuonExtras,"standaloneMuons\\3_standaloneMuons");
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\3_standaloneMuons.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\3_standaloneMuons.obj", dataList);
         }
@@ -376,12 +468,13 @@ namespace IGtoOBJGen
             }
             int firstassoc = assocs[0][1][1].Value<int>();
             subTrackExtras = trackExtrasData.GetRange(firstassoc, assocs.Last()[1][1].Value<int>() - firstassoc + 1);
+
             return dataList;
         }
         public void makeTracks() 
         {
             if (subTrackExtras == null) { return; }
-            List<string> dataList = trackCubicBezierCurve(subTrackExtras);
+            List<string> dataList = singleFiletrackCubicBezierCurve(subTrackExtras);
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\9_tracks.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\9_tracks.obj", dataList);
         }
@@ -420,15 +513,20 @@ namespace IGtoOBJGen
         }
         public void makeElectrons() 
         {
+            DirectoryInfo dir = new DirectoryInfo($"{desktopPath}\\{eventTitle}\\gsfElectrons");
+            foreach (var file in dir.GetFiles())
+            {
+                file.Delete();
+            }
             if (electronExtras == null) { return; }
-            List<string> dataList = trackCubicBezierCurve(electronExtras);
+            List<string> dataList = trackCubicBezierCurve(electronExtras, "gsfElectrons\\4_gsfElectrons");
             File.WriteAllText($"{desktopPath}\\{eventTitle}\\4_gsfElectrons.obj", String.Empty);
             File.WriteAllLines($"{desktopPath}\\{eventTitle}\\4_gsfElectrons.obj", dataList);
         }
         public METData METParse()
         {
             METData met = new METData();
-            var metdata = data["PFMETs_V1"][0];
+            var metdata = data["Collections"]["PFMETs_V1"][0];
             var children = metdata.Values<double>().ToList();
             met.phi = children[0];
             met.pt = children[1];
